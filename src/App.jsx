cd501bd2f6e67a3a5357be81613be161da733ce4
LaloLanda2004 <lazarofrance2004@gmail.com>
2023-10-20 15:14:08 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+
+  let nombre = "lazaro";
+  let paginas = "noticias"
+
+  return (
+
+    <>
+    <h1>¡Hola Mundo!</h1>
+{/* 
+    <h1>¡Hola soy {nombre}!</h1>
+
+    <p>te queria decir que me gustaria hacer una pagina de {paginas} </p> */}
+    </>
+  )
+}
+
+export default App
