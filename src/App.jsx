47a8a9506cfbe4e17612cf34ce0b1e60629e456d
LaloLanda2004 <lazarofrance2004@gmail.com>
2023-10-20 15:22:30 -0300

    segundo commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,10 @@ function App() {
 
     <>
     <h1>¡Hola Mundo!</h1>
-{/* 
+
     <h1>¡Hola soy {nombre}!</h1>
 
-    <p>te queria decir que me gustaria hacer una pagina de {paginas} </p> */}
+    <p>te queria decir que me gustaria hacer una pagina de {paginas} </p>
     </>
   )
 }
